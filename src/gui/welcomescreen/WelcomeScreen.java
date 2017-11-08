3824ea8e6dd029302ddb1f3fdb596b607d275328
Samarth_Desai <smd67@duke.edu>
2017-11-08 16:12:39 -0500

    Finished welcome screen without transitions, code still needs refactoring

--- src/gui/welcomescreen/WelcomeScreen.java
+++ src/gui/welcomescreen/WelcomeScreen.java
@@ -2,6 +2,10 @@ package gui.welcomescreen;
 
 import java.io.InputStream;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -21,7 +25,7 @@ import javafx.stage.Stage;
 
 public class WelcomeScreen {
 
-	public static final int WIDTH = 1200;
+	public static final int WIDTH = 1000;
 	public static final int HEIGHT = 700;
 	private static final String STAGE_TITLE = "VOOGA";
 	private static final String WELCOME_BACKGROUND_COLOR = "#001E32;";
@@ -41,46 +45,50 @@ public class WelcomeScreen {
 	private static final String MOTTO_FONT = TITLE_FONT;
 	private static final String MOTTO_SIZE = 16 + "pt;";
 	private static final String PLAY_STATIC_PATH = "Play_Static.png";
-	private static final int PLAY_STATIC_WIDTH = 200;
-	private static final int PLAY_STATIC_HEIGHT = 200;
 	private static final String PLAY_PATH = "Play.gif";
 	private static final int PLAY_WIDTH = 150;
 	private static final int PLAY_HEIGHT = 150;
-	private static final String BUILD_STATIC_PATH = "Build_Static.png";
-	private static final int BUILD_STATIC_WIDTH = 200;
-	private static final int BUILD_STATIC_HEIGHT = 200;
-	private static final String BUILD_PATH = "Build.gif";
-	private static final int BUILD_WIDTH = 150;
-	private static final int BUILD_HEIGHT = 150;
+	private static final String CREATE_STATIC_PATH = "Build_Static.png";
+	private static final String CREATE_PATH = "Build.gif";
+	private static final int CREATE_WIDTH = 150;
+	private static final int CREATE_HEIGHT = 150;
 	private static final String LEARN_STATIC_PATH = "Learn_Static.png";
-	private static final int LEARN_STATIC_WIDTH = 200;
-	private static final int LEARN_STATIC_HEIGHT = 200;
 	private static final String LEARN_PATH = "Learn.gif";
 	private static final int LEARN_WIDTH = 150;
 	private static final int LEARN_HEIGHT = 150;
 	private static final String SETTINGS_STATIC_PATH = "Settings_Static.png";
-	private static final int SETTINGS_STATIC_WIDTH = 200;
-	private static final int SETTINGS_STATIC_HEIGHT = 200;
 	private static final String SETTINGS_PATH = "Settings.gif";
 	private static final int SETTINGS_WIDTH = 150;
 	private static final int SETTINGS_HEIGHT = 150;
 	private static final int OPTIONS_HORIZONTAL_GAP = 100;
 	private static final int OPTIONS_BOTTOM_PADDING = 50;
-	private static final String OPTIONS_BOX_BORDER_COLOR = "blue";
+	private static final String OPTIONS_BOX_BORDER_COLOR = TITLE_COLOR;
+	private static final String PLAY_CAPTION = "Play";
+	private static final String CREATE_CAPTION = "Create";
+	private static final String LEARN_CAPTION = "Learn";
+	private static final String SETTINGS_CAPTION = "Settings";
 
 	private Stage stage;
 	private BorderPane rootPane;
 	
+	private Image playImage;
+	private Image playStaticImage;
 	private ImageView play;
-	private ImageView build;
+	private Image createImage;
+	private Image createStaticImage;
+	private ImageView create;
+	private Image learnImage;
+	private Image learnStaticImage;
 	private ImageView learn;
+	private Image settingsImage;
+	private Image settingsStaticImage;
 	private ImageView settings;
 	private HBox playHBox;
-	private HBox buildHBox;
+	private HBox createHBox;
 	private HBox learnHBox;
 	private HBox settingsHBox;
 	private VBox playVBox;
-	private VBox buildVBox;
+	private VBox createVBox;
 	private VBox learnVBox;
 	private VBox settingsVBox;
 	
@@ -117,7 +125,8 @@ public class WelcomeScreen {
 	}
 	
 	private HBox createTitle() {
-		ImageView infinity = createImageView(INFINITY_PATH, INFINITY_WIDTH, INFINITY_HEIGHT);
+		Image infinityImage = createImage(INFINITY_PATH, INFINITY_WIDTH, INFINITY_HEIGHT);
+		ImageView infinity = createImageView(infinityImage);
 		Label leftTitle = labelGenerator(LEFT_SEGMENT_TITLE, TITLE_FONT, TITLE_COLOR, TITLE_SIZE);
 		Label rightTitle = labelGenerator(RIGHT_SEGMENT_TITLE, TITLE_FONT, TITLE_COLOR, TITLE_SIZE);
 		
@@ -132,8 +141,11 @@ public class WelcomeScreen {
 		return titleBox;
 	}
 	
-	private ImageView createImageView(String path, int width, int height) {
+	private Image createImage(String path, int width, int height) {
 		Image image = new Image(WelcomeScreen.class.getClassLoader().getResourceAsStream(path), width, height, true, true);
+		return image;
+	}
+	private ImageView createImageView(Image image) {
 		ImageView imageView = new ImageView(image);
 		return imageView;
 	}
@@ -169,7 +181,7 @@ public class WelcomeScreen {
 		createWelcomeImageViews();
 		
 		HBox optionBox = new HBox(OPTIONS_HORIZONTAL_GAP);
-		optionBox.getChildren().addAll(playVBox, buildVBox, learn, settings);
+		optionBox.getChildren().addAll(playVBox, createVBox, learnVBox, settingsVBox);
 		optionBox.setAlignment(Pos.CENTER);
 		optionBox.setPadding(new Insets(0, 0, OPTIONS_BOTTOM_PADDING, 0));
 		return optionBox;
@@ -177,15 +189,33 @@ public class WelcomeScreen {
 	
 	private void createWelcomeImageViews() {
 		
-		play = createImageView(PLAY_PATH, PLAY_WIDTH, PLAY_HEIGHT);
-		build = createImageView(BUILD_PATH, BUILD_WIDTH, BUILD_HEIGHT);
-		learn = createImageView(LEARN_STATIC_PATH, LEARN_WIDTH, LEARN_HEIGHT);
-		settings = createImageView(SETTINGS_PATH, SETTINGS_WIDTH, SETTINGS_HEIGHT);
-		
+		createOptionImages();
+		initializeOptionImageViews();
 		createImageBorders();
 		createWelcomeBoxes();
 		handleHover();
 		
+	}
+	
+	private void createOptionImages(){
+		
+		playImage = createImage(PLAY_PATH, PLAY_WIDTH, PLAY_HEIGHT);
+		createImage = createImage(CREATE_PATH, CREATE_WIDTH, CREATE_HEIGHT);
+		learnImage = createImage(LEARN_PATH, LEARN_WIDTH, LEARN_HEIGHT);
+		settingsImage = createImage(SETTINGS_PATH, SETTINGS_WIDTH, SETTINGS_HEIGHT);
+		
+		playStaticImage = createImage(PLAY_STATIC_PATH, PLAY_WIDTH, PLAY_HEIGHT);
+		createStaticImage = createImage(CREATE_STATIC_PATH, CREATE_WIDTH, CREATE_HEIGHT);
+		learnStaticImage = createImage(LEARN_STATIC_PATH, LEARN_WIDTH, LEARN_HEIGHT);
+		settingsStaticImage = createImage(SETTINGS_STATIC_PATH, SETTINGS_WIDTH, SETTINGS_HEIGHT);
+	}
+	
+	private void initializeOptionImageViews() {
+		
+		play = createImageView(playStaticImage);
+		create = createImageView(createStaticImage);
+		learn = createImageView(learnStaticImage);
+		settings = createImageView(settingsStaticImage);
 	}
 	
 	private void createImageBorders() {
@@ -197,29 +227,56 @@ public class WelcomeScreen {
 		playHBox.getChildren().add(play);
 		playHBox.setStyle(styleBox(OPTIONS_BOX_BORDER_COLOR));
 		
-		buildHBox = new HBox();
-		buildHBox.getChildren().add(build);
-		buildHBox.setStyle(styleBox(OPTIONS_BOX_BORDER_COLOR));
+		createHBox = new HBox();
+		createHBox.getChildren().add(create);
+		createHBox.setStyle(styleBox(OPTIONS_BOX_BORDER_COLOR));
+		
+		learnHBox = new HBox();
+		learnHBox.getChildren().add(learn);
+		learnHBox.setStyle(styleBox(OPTIONS_BOX_BORDER_COLOR));
+		
+		settingsHBox = new HBox();
+		settingsHBox.getChildren().add(settings);
+		settingsHBox.setStyle(styleBox(OPTIONS_BOX_BORDER_COLOR));
+		
 	}
 	
 	private void createWelcomeBoxes() {
-		playVBox = optionBoxGenerator();
-		buildVBox = optionBoxGenerator2();
+		playVBox = playBoxGenerator();
+		createVBox = createBoxGenerator();
+		learnVBox = learnBoxGenerator();
+		settingsVBox = settingsBoxGenerator();
 	}
 	
-	private VBox optionBoxGenerator() {
+	private VBox playBoxGenerator() {
 		VBox box = new VBox();
 		box.setAlignment(Pos.CENTER);
-		Label playLabel = labelGenerator("Play", MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
-		box.getChildren().addAll(playLabel, playHBox);
+		Label label = labelGenerator(PLAY_CAPTION, MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
+		box.getChildren().addAll(label, playHBox);
 		return box;
 	}
 	
-	private VBox optionBoxGenerator2() {
+	private VBox createBoxGenerator() {
 		VBox box = new VBox();
 		box.setAlignment(Pos.CENTER);
-		Label playLabel = labelGenerator("Build", MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
-		box.getChildren().addAll(playLabel, buildHBox);
+		Label label = labelGenerator(CREATE_CAPTION, MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
+		box.getChildren().addAll(label, createHBox);
+		return box;
+	}
+	
+	private VBox learnBoxGenerator() {
+		VBox box = new VBox();
+		box.setAlignment(Pos.CENTER);
+		Label label = labelGenerator(LEARN_CAPTION, MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
+		box.getChildren().addAll(label, learnHBox);
+		return box;
+	}
+	
+	private VBox settingsBoxGenerator() {
+		VBox box = new VBox();
+		box.setAlignment(Pos.CENTER);
+		Label label = labelGenerator(SETTINGS_CAPTION, MOTTO_FONT, MOTTO_COLOR, MOTTO_SIZE);
+		box.getChildren().addAll(label, settingsHBox);
 		return box;
 	}
 	
@@ -236,17 +293,48 @@ public class WelcomeScreen {
 	}
 	
 	private void handleHover() {
-		play.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> handleEnter());
-		play.addEventHandler(MouseEvent.MOUSE_EXITED, e -> handleExit());
+		play.setOnMouseEntered(e -> playMouseEnter());
+		play.setOnMouseExited(e -> playMouseExit());
+		create.setOnMouseEntered(e -> createMouseEnter());
+		create.setOnMouseExited(e -> createMouseExit());
+		learn.setOnMouseEntered(e -> learnMouseEnter());
+		learn.setOnMouseExited(e -> learnMouseExit());
+		settings.setOnMouseEntered(e -> settingsMouseEnter());
+		settings.setOnMouseExited(e -> settingsMouseExit());
 	}
 	
-	private void handleEnter() {
-		play = createImageView(PLAY_PATH, PLAY_WIDTH, PLAY_HEIGHT);
+	private void playMouseEnter() {
+		play.setImage(playImage);
 	}
 	
-	private void handleExit() {
-		play = createImageView(PLAY_STATIC_PATH, PLAY_WIDTH, PLAY_HEIGHT);
+	private void playMouseExit() {
+		play.setImage(playStaticImage);
 	}
+	
+	private void createMouseEnter() {
+		create.setImage(createImage);
+	}
+	
+	private void createMouseExit() {
+		create.setImage(createStaticImage);
+	}
+	
+	private void learnMouseEnter() {
+		learn.setImage(learnImage);
+	}
+	
+	private void learnMouseExit() {
+		learn.setImage(learnStaticImage);
+	}
+	
+	private void settingsMouseEnter() {
+		settings.setImage(settingsImage);
+	}
+	
+	private void settingsMouseExit() {
+		settings.setImage(settingsStaticImage);
+	}
+	
 	
 	private void createTransition() {
 		
