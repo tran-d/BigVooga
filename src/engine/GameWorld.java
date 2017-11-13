4064888df515ddaec479addb0470eec3b5bec4be
Nikolas Bramblett <nlb27@duke.edu>
2017-11-13 14:23:20 -0500

    Set up the basic classes for us to fill.

---
+++ src/engine/GameWorld.java
@@ -0,0 +1,36 @@
+package engine;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class GameWorld implements World{
+
+	public GameWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Iterator<GameObject> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addGameObject(GameObject obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeGameObject(GameObject obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<GameObject> getWithTag(String tag) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/engine/GenericObject.java
@@ -0,0 +1,100 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GenericObject implements GameObject{
+	
+	private Set<String> tagSet;
+	public GenericObject() {
+		// TODO Auto-generated constructor stub
+		tagSet = new HashSet<String>();
+	}
+
+	@Override
+	public void addTag(String tag) {
+		// TODO Auto-generated method stub
+		tagSet.add(tag);
+	}
+
+	@Override
+	public boolean is(String tag) {
+		// TODO Auto-generated method stub
+		return tagSet.contains(tag);
+	}
+
+	@Override
+	public List<String> getTags() {
+		// TODO Auto-generated method stub
+		return new ArrayList<String>(tagSet);
+	}
+
+	@Override
+	public void setGlobal(String variableName, boolean global) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeAllGlobal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIntegerVariable(String name, int val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDoubleVariable(String name, double val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStringVariable(String name, String val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getIntegerVariable(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getDoubleVariable(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getStringVariable(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addToObjectList(GameObject o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addConditionAction(Condition c, Action a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
