8d66760f974bbb65a45cfaa9089025b4d3ebf7b9
Dara <deb26@duke.edu>
2017-11-17 00:55:47 -0500

    added functionality logic to param factory

--- src/authoring_UI/FEParameterFactory.java
+++ src/authoring_UI/FEParameterFactory.java
@@ -1,9 +1,34 @@
 package authoring_UI;
 
-public class FEParameterFactory {
+import java.util.ArrayList;
 
-	public FEParameterFactory() {
-		
+import authoring.BooleanSpriteParameter;
+import authoring.DoubleSpriteParameter;
+import authoring.SpriteParameterI;
+import authoring.StringSpriteParameter;
+import javafx.scene.layout.VBox;
+
+public class FEParameterFactory extends VBox {
+
+	protected FEParameterFactory(ArrayList<SpriteParameterI> spriteParams) {
+		for (SpriteParameterI BEParam : spriteParams) {
+			createFEParameter(BEParam);
+		}
 	}
+	
+	private void createFEParameter(SpriteParameterI BEParam) {
+		if (BEParam instanceof BooleanSpriteParameter) {
+			FEBooleanParameter newBoolean = new FEBooleanParameter(BEParam);
+			this.getChildren().add(newBoolean);
+		} else if (BEParam instanceof StringSpriteParameter) {
+			FEStringParameter newString = new FEStringParameter(BEParam);
+			this.getChildren().add(newString);
+		} else if (BEParam instanceof DoubleSpriteParameter) {
+			FEDoubleParameter newDouble = new FEDoubleParameter(BEParam);
+			this.getChildren().add(newDouble);
+		}
+	}
+	
+	
 	
 }
