c794ef5dea56ea9375e76cd3c0cb175e8441f712
Samarth_Desai <smd67@duke.edu>
2017-11-20 18:14:05 -0500

    Commented the DisplayLanguage class

--- src/tools/DisplayLanguage.java
+++ src/tools/DisplayLanguage.java
@@ -17,6 +17,7 @@ import java.util.concurrent.Callable;
 /**
  * This class mediates the change of display language in the GUI by providing numerous static classes that create binded labels and buttons,
  * along with the internal methods that facilitate the change of language among these GUI objects that display text.
+ * 
  * @author Samarth Desai
  *
  */
@@ -32,60 +33,78 @@ public final class DisplayLanguage {
     }
     /**
      * Populates and gets the list of the supported Locales.
-     * @return The list of Locale objects that have been accounted for.
+     * 
+     * @return the list of Locale objects that have been accounted for.
      */
     public static List<Locale> getSupportedLocales() {
         return new ArrayList<>(Arrays.asList(Locale.ENGLISH, new Locale("es")));
     }
     /**
-     * get the default locale. This is the systems default if contained in the supported locales, english otherwise.
+     * Gets the default locale. This is the systems default if contained in the supported locales, but is english otherwise.
      *
-     * @return
+     * @return the default locale as long as it is supported
      */
     public static Locale getDefaultLocale() {
         Locale sysDefault = Locale.getDefault();
         return getSupportedLocales().contains(sysDefault) ? sysDefault : Locale.ENGLISH;
     }
+    
+    /**
+     * Gets the current locale.
+     * 
+     * @return The current locale
+     */
     public static Locale getLocale() {
         return locale.get();
     }
+    
+    /**
+     * Sets the new locale after a language button is clicked.
+     * 
+     * @param locale - The locale that is being set
+     */
     public static void setLocale(Locale locale) {
         localeProperty().set(locale);
         Locale.setDefault(locale);
     }
+    
+    /**
+     * Allows dynamic changing of the local using the ObjectProperty.
+     * 
+     * @return the changed locale
+     */
     public static ObjectProperty<Locale> localeProperty() {
         return locale;
     }
+    
     /**
-     * gets the string with the given key from the resource bundle for the current locale and uses it as first argument
+     * Gets the string with the given key from the resource bundle for the current locale and uses it as first argument
      * to MessageFormat.format, passing in the optional args and returning the result.
      *
-     * @param key
-     *         message key
-     * @param args
-     *         optional arguments for the message
-     * @return localized formatted string
+     * @param key - The message key
+     * @param args - Optional arguments for the message
+     * @return The localized formatted string
      */
     public static String get(final String key, final Object... args) {
         ResourceBundle bundle = ResourceBundle.getBundle("languages/messages_" + getLocale());
         return MessageFormat.format(bundle.getString(key), args);
     }
+    
     /**
-     * creates a String binding to a localized String for the given message bundle key
+     * Creates a String binding to a localized String for the given message bundle key.
      *
-     * @param key
-     *         key
-     * @return String binding
+     * @param key - The language string
+     * @return the bound language string
      */
     public static StringBinding createStringBinding(final String key, Object... args) {
         return Bindings.createStringBinding(() -> get(key, args), locale);
     }
+    
     /**
-     * creates a String Binding to a localized String that is computed by calling the given func
+     * Creates a String Binding to a localized String that is computed by calling the given func.
      *
-     * @param func
-     *         function called on every change
-     * @return StringBinding
+     * @param func - The function called on every change
+     * @return the bound language string
      */
     public static StringBinding createStringBinding(Callable<String> func) {
         return Bindings.createStringBinding(func, locale);
