54c3e2c0da8997f950ad4f3ecf0ad7a743409c6d
Owen Smith <os29@duke.edu>
2017-11-20 18:09:39 -0500

    Almost done integrating changes

---
+++ src/authoring_UI/RemoveChoiceBox.java
@@ -0,0 +1,31 @@
+package authoring_UI;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ChoiceBox;
+
+public class RemoveChoiceBox extends ChoiceBox<Integer> {
+	
+	ObservableList<Integer> currentRows;
+	
+	public RemoveChoiceBox() {
+		super();
+		currentRows = FXCollections.observableArrayList();
+		currentRows.addListener((ListChangeListener<Integer>) c -> setItems(currentRows));
+	}
+	
+	protected void addRow() {
+		if(currentRows.isEmpty()) {
+			currentRows.add(1);
+		}
+		else {
+			currentRows.add(currentRows.size() + 1);
+		}
+	}
+	
+	protected void removeRow(int row) {
+		currentRows.remove(row);
+	}
+	
+}
