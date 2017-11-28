5e1f88a0d81c831facefccba0efea80a37401c8e
Ian Eldridge-Allegra <ike@duke.edu>
2017-11-27 22:12:19 -0500

    Bugs Resolved

--- src/engine/GameMaster.java
+++ src/engine/GameMaster.java
@@ -20,15 +20,15 @@ public class GameMaster implements EngineController{
 	private static final int DEFAULT_FPS = 60;
 	private static final int DEFAULT_DELAY = 1000/DEFAULT_FPS;
 	
-	private World currentWorld;
-	private List<World> madeWorlds;
+	private GameWorld currentWorld;
+	private List<GameWorld> madeWorlds;
 	private Timeline gameLoop;
 	private GlobalVariables globalVars;
 	private PlayerManager playerManager;
 	
 	public GameMaster() {
 		// TODO Auto-generated constructor stub
-		madeWorlds = new ArrayList<World>();
+		madeWorlds = new ArrayList<>();
 		
 		
 		globalVars = new GlobalVariables();
@@ -56,7 +56,7 @@ public class GameMaster implements EngineController{
 	}
 
 	@Override
-	public void addWorld(World w) {
+	public void addWorld(GameWorld w) {
 		// TODO Auto-generated method stub
 		w.addGlobalVars(globalVars);
 		w.setPlayerManager(playerManager);
@@ -66,7 +66,7 @@ public class GameMaster implements EngineController{
 	@Override
 	public void setCurrentWorld(String s) {
 		// TODO Auto-generated method stub
-		for(World w : madeWorlds) {
+		for(GameWorld w : madeWorlds) {
 			if(w.isNamed(s)) {
 				currentWorld = w;
 				return;
@@ -78,7 +78,7 @@ public class GameMaster implements EngineController{
 	}
 	
 	private void step() {
-		currentWorld = ((GameLayer)currentWorld).getNextWorld();
+		currentWorld = currentWorld.getNextWorld();
 		currentWorld.step();
 		imageUpdate();
 		playerManager.step();
@@ -87,7 +87,7 @@ public class GameMaster implements EngineController{
 	@Override
 	public void setPlayerManager(PlayerManager currentPlayerManager) {
 		playerManager = currentPlayerManager;
-		for(World w : madeWorlds)
+		for(GameWorld w : madeWorlds)
 			w.setPlayerManager(playerManager);
 	}
 	
@@ -102,7 +102,6 @@ public class GameMaster implements EngineController{
 			imageData.add(o.getImage());
 		}
 		Collections.sort(imageData);
-		
 		playerManager.setImageData(imageData);
 	}
 }
