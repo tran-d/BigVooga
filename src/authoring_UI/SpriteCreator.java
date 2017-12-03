0e0367192351d2f7dfe723489975f76ae8e275a8
Ryan Chung <tc140@duke.edu>
2017-12-03 17:29:43 -0500

    spritecreator UI completed

--- src/authoring_UI/SpriteCreator.java
+++ src/authoring_UI/SpriteCreator.java
@@ -1,9 +1,32 @@
 package authoring_UI;
 
-import gui.welcomescreen.WelcomeScreen;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * Class for creating new sprites
@@ -14,22 +37,161 @@ import javafx.scene.layout.GridPane;
 
 public class SpriteCreator {
 
-	private TabPane myTabPane;
+	public static final String PATH = "resources/";
+	private static final int PANE_WIDTH = 1300;
+	private static final int PANE_HEIGHT = 600;
 
+	private TabPane myTabPane;
+	private VBox myStatePanel;
+	private StackPane myImageStack;
+	private int spriteCount = 1;
 
 	public SpriteCreator() {
 		myTabPane = new TabPane();
-		Tab tab = new Tab();
-		tab.setText("test");
-		
-		GridPane gp = new GridPane();
-		gp.setPrefWidth(WelcomeScreen.WIDTH - ViewSideBar.VIEW_MENU_HIDDEN_WIDTH);
-		gp.setPrefHeight(WelcomeScreen.HEIGHT);
-		tab.setContent(gp);
-		
+
+		Tab tab = makeTab();
 		myTabPane.getTabs().add(tab);
-//		myTabPane.setPrefWidth(WelcomeScreen.WIDTH - ViewSideBar.VIEW_MENU_HIDDEN_WIDTH);
-//		myTabPane.setPrefHeight(WelcomeScreen.HEIGHT);
+
+	}
+
+	private Tab makeTab() {
+		Tab tab = new Tab();
+		tab.setText("sprite" + spriteCount);
+		spriteCount++;
+
+		HBox hb = addParentHBox(tab);
+
+		addStatePanel(hb);
+		addImageStackPane(hb);
+		addToolBox(hb);
+
+		return tab;
+
+	}
+
+	private void addToolBox(HBox parentBox) {
+		VBox toolBox = new VBox();
+		toolBox.setPrefSize(200, PANE_HEIGHT);
+		Button b1 = new Button("test tool_1");
+		Button b2 = new Button("test tool_2");
+		toolBox.getChildren().addAll(b1,b2);
+		parentBox.getChildren().add(toolBox);
+	}
+
+	private void addImageStackPane(HBox parentBox) {
+		myImageStack = new StackPane();
+		myImageStack.setPrefSize(PANE_WIDTH/2-100, PANE_HEIGHT);
+		myImageStack.setMaxSize(PANE_WIDTH/2-100, PANE_HEIGHT);
+		myImageStack.setBackground(
+				new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		BorderStroke border = new BorderStroke(Color.LIGHTGREY, BorderStrokeStyle.DOTTED,
+				CornerRadii.EMPTY, BorderWidths.DEFAULT);
+		myImageStack.setBorder(new Border(border));
+		ImageView test = new ImageView("pikachu.png");
+		myImageStack.getChildren().add(test);
+		parentBox.getChildren().add(myImageStack);
+//		parentBox.getChildren().add(new Text("test"));
+	}
+
+	private void addStatePanel(HBox parentBox) {
+		myStatePanel = new VBox();
+		myStatePanel.setPrefWidth(PANE_WIDTH / 2);
+		myStatePanel.setMaxWidth(PANE_WIDTH / 2);
+		myStatePanel.setPrefHeight(PANE_HEIGHT);
+		myStatePanel.setMaxWidth(PANE_WIDTH);
+		myStatePanel.setStyle("-fx-background-color: #ffaadd;");
+
+		HBox buttonBox = new HBox(10);
+		
+		addButtons(buttonBox);
+		VBox stateBox = createStateBox();
+		ScrollPane sp = createGrid();
+
+		myStatePanel.getChildren().addAll(buttonBox, stateBox, sp);
+
+		parentBox.getChildren().add(myStatePanel);
+	}
+
+	private void addButtons(HBox buttonBox) {
+		Button loadImageButton = new Button("Load Image");
+		loadImageButton.setOnAction(e -> {
+			try {
+				openImage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+		
+		Button createSpriteButton = new Button("Create Sprite Template");
+		
+		buttonBox.getChildren().addAll(loadImageButton, createSpriteButton);
+
+	}
+
+	private void openImage() throws IOException {
+		FileChooser imageChooser = new FileChooser();
+		imageChooser.setInitialDirectory(new File("resources/"));
+		imageChooser.setTitle("Open Image");
+		File file = imageChooser.showOpenDialog(new Stage());
+
+		if (file != null) {
+			Files.copy(file.toPath(), Paths.get(PATH + file.getName()), StandardCopyOption.REPLACE_EXISTING);
+			Image image = new Image(file.getName());
+			ImageView imageView = new ImageView(image);
+			imageView.setFitHeight(PANE_HEIGHT);
+			imageView.setFitWidth(PANE_WIDTH/2-100);
+			myImageStack.getChildren().remove(0);
+			myImageStack.getChildren().add(imageView);
+			
+//			myStatePanel.getChildren().add(imageView);
+			System.out.println("image loaded");
+			// System.out.println(file.getName());
+			// mySpriteObject.setImageURL(file.getName());
+			// setChanged();
+			// System.out.print(file.getName());
+			// notifyObservers(file.getName());
+			// System.out.println("image chosen");
+		}
+	}
+
+	private ScrollPane createGrid() {
+		GridPane gp = new GridPane();
+
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 10; j++) {
+				StackPane sp = new StackPane();
+				sp.setPrefHeight(50);
+				sp.setPrefWidth(50);
+				sp.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+				BorderStroke border = new BorderStroke(Color.LIGHTGREY, BorderStrokeStyle.DOTTED, CornerRadii.EMPTY,
+						BorderWidths.DEFAULT);
+				sp.setBorder(new Border(border));
+				gp.add(sp, i, j);
+			}
+		}
+
+		ScrollPane sp = new ScrollPane(gp);
+		sp.setMaxHeight(PANE_HEIGHT/2);
+		return sp;
+	}
+
+	private VBox createStateBox() {
+		VBox stateBox = new VBox();
+		stateBox.setMaxHeight(PANE_HEIGHT / 2);
+		stateBox.setPrefHeight(PANE_HEIGHT / 2);
+		stateBox.setPrefWidth(PANE_WIDTH / 2);
+		stateBox.setStyle("-fx-background-color: #ffaadd;");
+		return stateBox;
+	}
+
+	private HBox addParentHBox(Tab tab) {
+		HBox hb = new HBox();
+		hb.setStyle("-fx-background-color: #FFFFFF;");
+		hb.setPrefWidth(PANE_WIDTH);
+		hb.setPrefHeight(PANE_HEIGHT);
+		tab.setContent(hb);
+		return hb;
 	}
 
 	public TabPane getParent() {
