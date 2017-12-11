6cfd71bafebc9f416810964673ad41178db1a14f
Aaron <anp36@duke.edu>
2017-12-10 22:54:12 -0500

    Cleaning up TODOs, added getters to VariableContainer for DataView #49

--- src/engine/VariableContainer.java
+++ src/engine/VariableContainer.java
@@ -20,41 +20,37 @@ public abstract class VariableContainer {
 	private static final int DEFAULT_DOUBLE = 0;
 
 	protected Map<String, Double> doubleVars;
-	protected Map<String, List<Double>> doubleListVars;
-
 	protected Map<String, Point2D> vectorVars;
-
 	protected Map<String, String> stringVars;
-	protected Map<String, List<String>> stringListVars;
-
 	protected Map<String, Boolean> booleanVars;
-	
-	protected Map<String, GameObject> inventory;
-	protected VariableContainer holder;
 
 	public VariableContainer() {
 		doubleVars = new HashMap<String, Double>();
-		doubleListVars = new HashMap<String, List<Double>>();
-
 		vectorVars = new HashMap<String, Point2D>();
-
 		stringVars = new HashMap<String, String>();
-		stringListVars = new HashMap<String, List<String>>();
-
 		booleanVars = new HashMap<String, Boolean>();
-		
-		inventory = new HashMap<String, GameObject>();
-		holder = null;
 	}
-
+	
+	public Map<String, Double> getAllDoubleVars() {
+		return doubleVars;
+	}
+	
+	public Map<String, Point2D> getAllVectorVars() {
+		return vectorVars;
+	}
+	
+	public Map<String, String> getAllStringVars() {
+		return stringVars;
+	}
+	
+	public Map<String, Boolean> getAllBooleanVars() {
+		return booleanVars;
+	}
+	
 	public double getDouble(String key) {
 		if (doubleVars.containsKey(key))
 			return doubleVars.get(key);
 		return DEFAULT_DOUBLE;
-	}
-
-	public List<Double> getDoubleList(String key) {
-		return doubleListVars.get(key);
 	}
 
 	public Point2D getVector(String key) {
@@ -67,10 +63,6 @@ public abstract class VariableContainer {
 		return DEFAULT_STRING;
 	}
 
-	public List<String> getStringList(String key) {
-		return stringListVars.get(key);
-	}
-
 	public boolean getBoolean(String key) {
 		if (booleanVars.containsKey(key))
 			return booleanVars.get(key);
@@ -81,10 +73,6 @@ public abstract class VariableContainer {
 		doubleVars.put(name, val);
 	}
 
-	public void setDoubleListVariable(String name, List<Double> val) {
-		doubleListVars.put(name, val);
-	}
-
 	public void setVectorVariable(String name, Point2D val) {
 		vectorVars.put(name, val);
 	}
@@ -93,19 +81,7 @@ public abstract class VariableContainer {
 		stringVars.put(name, val);
 	}
 
-	public void setStringListVariable(String varName, List<String> val) {
-		stringListVars.put(varName, val);
-	}
-
 	public void setBooleanVariable(String name, boolean val) {
 		booleanVars.put(name, val);
-	}
-	
-	public void setGameObjectVariable(String name, GameObject val) {
-		inventory.put(name, val);
-	}
-	
-	public void setHolder(VariableContainer holder) {
-		this.holder = holder;
 	}
 }
