fba55feeb5b983f3b0cefc948a4abf1df28ef01b
Nikolas Bramblett <nlb27@duke.edu>
2017-12-14 15:42:14 -0500

    Added a path-following system. Technically AI? #38

---
+++ src/engine/Actions/advanced/AdvancedAnimation.java
@@ -0,0 +1,19 @@
+package engine.Actions.advanced;
+
+import engine.Actions.changeObject.SetAnimationSequence;
+import engine.operations.VoogaAnnotation;
+import engine.operations.VoogaType;
+import engine.operations.gameobjectops.GameObjectOperation;
+import engine.operations.stringops.StringOperation;
+
+public class AdvancedAnimation extends SetAnimationSequence{
+
+	public AdvancedAnimation(@VoogaAnnotation(name = "Sprite", type = VoogaType.GAMEOBJECT) GameObjectOperation object,
+			@VoogaAnnotation(name = "Animation Sequence", type = VoogaType.STRING) StringOperation name) {
+		super(object, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
