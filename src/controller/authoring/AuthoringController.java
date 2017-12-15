ed648cffb4f4bba08bf0f6bd38b2961f67427704
Nikolas Bramblett <nlb27@duke.edu>
2017-12-15 01:50:14 -0500

    updated

--- src/controller/authoring/AuthoringController.java
+++ src/controller/authoring/AuthoringController.java
@@ -102,7 +102,7 @@ public class AuthoringController {
 		viewMap.put(CUTSCENES_KEY, cm.getPane());
 
 		DialogueManager dm = new DialogueManager(AEM);
-		dm.addDialogueListener(mapManager.getDialoguesTab());
+		//dm.addDialogueListener(mapManager.getDialoguesTab());
 		viewMap.put(DIALOGUE_KEY, dm.getPane());
 
 		HUDManager hudManager = new HUDManager(AEM, scene);
