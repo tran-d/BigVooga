c792e4a1e8da7c9be64cfb382fae5b9ad2b9392a
Owen Smith <os29@duke.edu>
2017-12-15 00:32:21 -0500

    Fixing small bug, #25

--- src/ActionConditionClasses/ActionCheckBoxVBoxI.java
+++ src/ActionConditionClasses/ActionCheckBoxVBoxI.java
@@ -3,6 +3,7 @@ package ActionConditionClasses;
 public interface ActionCheckBoxVBoxI {
 	
 	public void addAction();
+	public void removeAction(Integer action);
 	public Object getSelectedActions();
 
 }
