386c1f71947d49c4f0a5e5cf8c0c15e7ee067c66
Owen Smith <os29@duke.edu>
2017-12-15 01:25:02 -0500

    Added something, don't remember what

--- src/authoring/SpritePanels/DisplayPanel.java
+++ src/authoring/SpritePanels/DisplayPanel.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
 import ActionConditionClasses.ApplyButtonController;
 import ActionConditionClasses.ResourceBundleUtil;
 import authoring.AuthoringEnvironmentManager;
