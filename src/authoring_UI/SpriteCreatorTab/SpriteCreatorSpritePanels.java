bd10fd51644a2badd28fba628974726e66962ed8
Ryan Chung <tc140@duke.edu>
2017-12-18 00:08:27 -0500

    doc

--- src/authoring_UI/SpriteCreatorTab/SpriteCreatorSpritePanels.java
+++ src/authoring_UI/SpriteCreatorTab/SpriteCreatorSpritePanels.java
@@ -1,32 +1,36 @@
 package authoring_UI.SpriteCreatorTab;
 
-
 import authoring.SpritePanels.*;
 import authoring_UI.*;
 import authoring.*;
-public class SpriteCreatorSpritePanels extends SpritePanels{
-	
-	
-	SpriteCreatorSpritePanels(){
+
+/**
+ * SpriteCreatorSpritePanels extends SpritePanels which extends VBox. Contains
+ * DisplayPanel and SpriteCreatorElementSelector
+ * 
+ * @author taekwhunchung
+ *
+ */
+
+public class SpriteCreatorSpritePanels extends SpritePanels {
+
+	SpriteCreatorSpritePanels() {
 		super();
-		
+
 	}
-	
+
 	public SpriteCreatorSpritePanels(SpriteGridHandler mySGH, AuthoringEnvironmentManager myAEM, String type) {
 		super(mySGH, myAEM, type);
-		
+
 	}
-	
+
 	@Override
-	public void makeLayerDisplayPanel(AuthoringEnvironmentManager myAEM){
-		displayPanel = new DisplayPanel(SPSM, myAEM);  
+	public void makeLayerDisplayPanel(AuthoringEnvironmentManager myAEM) {
+		displayPanel = new DisplayPanel(SPSM, myAEM);
 	}
-	
-	
-	
+
 	@Override
-	public void makeElementSelector(SpriteGridHandler mySGH, AuthoringEnvironmentManager myAEM){
+	public void makeElementSelector(SpriteGridHandler mySGH, AuthoringEnvironmentManager myAEM) {
 		gameElementSelector = new SpriteCreatorElementSelector(mySGH, myAEM, myType);
 	}
 }
-
